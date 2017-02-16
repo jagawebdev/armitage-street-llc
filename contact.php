3de2dce56d325a801bf953dc2541711c3e46a472
jaga <jagawebdev@gmail.com>
2017-02-16 18:23:34 +0000

    new form

--- contact.php
+++ contact.php
@@ -1,57 +1,14 @@
 <?php
-$send_to = 'jagawebdev@gmail.com';
-
-$errors         = array();  	// array to hold validation errors 
-$data 			= array(); 		// array to pass back data
-
-// validate the variables ======================================================
-	// if any of these variables don't exist, add an error to our $errors array
-
-	if (empty($_POST['Name']))
-		$errors['name'] = 'Your Name is required.';
-	if (empty($_POST['Email']))
-		$errors['email'] = 'Your Email is required.';
-   	if (empty($_POST['Phone']))
-		$errors['phone'] = 'Your Phone Number is required.';
-
-// return a response ===========================================================
-
-	// if there are any errors in our errors array, return a success boolean of false
-	if ( ! empty($errors)) {
-
-		// if there are items in our errors array, return those errors
-		$data['success'] = false;
-		$data['errors']  = $errors;
-	} else {
-
-		// if there are no errors process our form, then return a message
-
-    	//If there is no errors, send the email to agent and client
-    	if( empty($errors) ) { 
-            
-            $title = htmlspecialchars_decode('{title}',ENT_QUOTES);    
-			$subject = $title . ' Contact Form (Bride: {bride_first_name} - Groom: {groom_first_name})';
-			$headers = 'From: ' . $send_to . "\r\n" .
-			    'Reply-To: ' . $send_to . "\r\n" .
-			    'X-Mailer: PHP/' . phpversion();
-
-        	$message = ' Subject: ' . $_POST['inputSubject'] . '
-                  
-            Name: ' . $_POST['Name'] . '
-            Email: ' . $_POST['Email'] . '
-            Phone: ' . $_POST['Phone'] . '            
-            Message: ' . $_POST['inputMessage'];
- 
-        	$headers = 'From: Contact Form' . '<' . $send_to . '>' . "\r\n" . 'Reply-To: ' . $_POST['inputEmail'];
-        	mail($send_to, $subject, $message, $headers);            
-            
-            }
-
-		// show a message of success and provide a true success variable
-		$data['success'] = true;
-		$data['message'] = 'Thank you!';
-        
-        }
-
-	// return all our data to an AJAX call
-	echo json_encode($data);
+    $to = "jagawebdev@gmail.com"; 
+    $from = $_REQUEST['email']; 
+    $name = $_REQUEST['name']; 
+    $headers = "From: $from"; 
+    $subject = "You have a message sent from your site"; 
+    $fields = array(); 
+    $fields{"name"} = "name"; 
+    $fields{"email"} = "email"; 
+    $fields{"phone"} = "phone"; 
+    $fields{"message"} = "message";
+    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+    $send = mail($to, $subject, $body, $headers);
+?>
--- js/contact.js
+++ js/contact.js
@@ -1,81 +1,51 @@
-// contact.js
-$(document).ready(function() {
-
-	// process the form 
-	$('#contactform form').submit(function(event) {
-
-		$('.form-group').removeClass('has-error'); // remove the error class
-		$('.help-block').remove(); // remove the error text
-
-        
-		// get the form data
-		// there are many ways to get this data using jQuery (you can use the class or id also)
-		var formData = {
-			'Name' 			        : $('input[name=inputName]').val(),
-			'Email' 			    : $('input[name=inputEmail]').val(),
-           	'Phone' 			    : $('input[name=inputPhone]').val(),
-            'inputSubject' 			: $('input[name=inputSubject]').val(),
-            'inputMessage' 			: $('textarea[name=inputMessage]').val()
-		};
-        
-        console.log(formData);
-
-		// process the form
-		$.ajax({
-			type 		: 'POST', // define the type of HTTP verb we want to use (POST for our form)
-			url 		: 'contact.php', // the url where we want to POST
-			data 		: formData, // our data object
-			dataType 	: 'json', // what type of data do we expect back from the server
-			encode 		: true
-		})
-			// using the done promise callback
-			.done(function(data) {
-
-				// log data to the console so we can see
-				console.log(data); 
-
-				// here we will handle errors and validation messages
-				if ( ! data.success) {
-					
-					// handle errors for name ---------------
-					if (data.errors.name) {
-						$('#cname-group').addClass('has-error'); // add the error class to show red input
-						$('#cname-group label').append('<span class="help-block">' + data.errors.name + '</span>'); // add the actual error message under our input
-					}
-
-					// handle errors for email ---------------
-					if (data.errors.email) {
-						$('#cemail-group').addClass('has-error'); // add the error class to show red input
-						$('#cemail-group label').append('<span class="help-block">' + data.errors.email + '</span>'); // add the actual error message under our input
-					}
-                    
-                    // handle errors for phone ---------------
-					if (data.errors.phone) {
-						$('#cphone-group').addClass('has-error'); // add the error class to show red input
-						$('#cphone-group label').append('<span class="help-block">' + data.errors.phone + '</span>'); // add the actual error message under our input
-					}
-
-				} else {
-
-					// ALL GOOD! just show the success message!
-					$('#contactform form').append('<div class="alert alert-success">' + data.message + '</div>');
-
-					// usually after form submission, you'll want to redirect
-					//window.location = '#bottom'; // redirect a user to another page
-
-				}
-			})
-
-			// using the fail promise callback
-			.fail(function(data) {
-
-				// show any errors
-				// best to remove for production
-				console.log(data);
-			});
-
-		// stop the form from submitting the normal way and refreshing the page
-		event.preventDefault();
-	});
-
+/* global $ */
+// validate contact form
+$(function() {
+    $('#contact').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            message: {
+                required: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Please enter your name!",
+                minlength: "your name must consist of at least 2 characters"
+            },
+            email: {
+                required: "Please enter valid email address!"
+            },
+            message: {
+                required: "Please include a message!"
+            }
+        },
+        submitHandler: function(form) {
+            $(form).ajaxSubmit({
+                type:"POST",
+                data: $(form).serialize(),
+                url:"contact.php",
+                success: function() {
+                    $('#contact :input').attr('disabled', 'disabled');
+                    $('#contact').fadeTo( "slow", 0.15, function() {
+                        $(this).find(':input').attr('disabled', 'disabled');
+                        $(this).find('label').css('cursor','default');
+                        $('#success').fadeIn();
+                    });
+                },
+                error: function() {
+                    $('#contact').fadeTo( "slow", 0.15, function() {
+                        $('#error').fadeIn();
+                    });
+                }
+            });
+        }
+    });
 });
