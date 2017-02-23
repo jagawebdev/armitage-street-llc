c73eece245c51e3c3ce35e2e4103233eb418b4d9
jaga <jagawebdev@gmail.com>
2017-02-23 21:10:47 +0000

    project submition form is done

--- js/project.php
+++ js/project.php
@@ -1,28 +1,28 @@
 <?php
-    $to = "jagawebdev@gmail.com"; 
-    $from = $_REQUEST['email']; 
-    $name = $_REQUEST['name']; 
-    $headers = "From: $from"; 
-    $subject = "You have a message sent from your site"; 
-    $fields = array(); 
-    $fields{"name"} = "name"; 
-    $fields{"phone"} = "phone";
-    $fields{"email"} = "email"; 
-    $fields{"hear-about-us"} = "hear-about-us";
-    $fields{"borrower-name"} = "borrower-name";
-    $fields{"entity-type"} = "entity-type";
-    $fields{"property-information"} = "property-information";
-    $fields{"address-one"} = "address-one";
-    $fields{"address-two"} = "address-two";
-    $fields{"city"} = "city";
-    $fields{"state"} = "state";
-    $fields{"zip-code"} = "zip-code";
-    $fields{"property-type"} = "property-type";
-    $fields{"purchase-price"} = "purchase-price";
-    $fields{"estimated-renovation-budget"} = "estimated-renovation-budget";
-    $fields{"target-exit-price"} = "target-exit-price";
-    $fields{"type-of-loan"} = "type-of-loan";
-    $fields{"notes"} = "notes";
-      $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+    $to = "origination@armitagest.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $headers = "From: $from";
+    $subject = "You have a message sent from your site";
+    $fields = array();
+    $fields{"name"} = "Name";
+    $fields{"phone"} = "Phone";
+    $fields{"email"} = "Email";
+    $fields{"hear-about-us"} = "How did you hear about us?";
+    $fields{"borrower-name"} = "Borrower Name";
+    $fields{"entity-type"} = "Entity Type";
+    $fields{"property-information"} = "Property Information";
+    $fields{"address-one"} = "Address 1";
+    $fields{"address-two"} = "Address 2";
+    $fields{"city"} = "City";
+    $fields{"state"} = "State";
+    $fields{"zip-code"} = "Zip Code";
+    $fields{"property-type"} = "Property Type";
+    $fields{"purchase-price"} = "Purchase Price";
+    $fields{"estimated-renovation-budget"} = "Estimated Renovation Budget";
+    $fields{"target-exit-price"} = "Target Exit Price";
+    $fields{"type-of-loan"} = "Type of Loan";
+    $fields{"notes"} = "Notes";
+    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
     $send = mail($to, $subject, $body, $headers);
 ?>
